31c0cf7f1e281bbcb191476479871f9b8329c092
Shuboi18 <agrawal.shubhay2002@gmail.com>
2026-02-09 14:08:55 +1100

    .get(*)

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 // Handle SPA 404 (Wildcard Route) - Must be after all API routes
 // Handle SPA 404 (Wildcard Route) - Must be after all API routes
-app.get(/(.*)/, (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
 });
 
